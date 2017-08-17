c02f6addb091d55190fc26250e6d8a20edd78270
Vernitrax <ty21@op.pl>
2017-08-17 10:04:06 +0200

    mergowanie nadchodzi

--- gittie.py
+++ gittie.py
@@ -1,16 +1,21 @@
 def welcome():
 	print("Hello, Im gittie!")
 
+
 def add():
 	sum=5+5
 	print (sum)
+
 	
-	
-welcome()
-add()
+def suchar():
+	print("Raz dwa trzy, git jest fajny")	
+
 
 def joke():
 	print("Dlaczego informatycy to dziwny typ ludzi? Bo trzymaja stringi w bibliotece xD xD")
 	
 
+welcome()
+add()
+suchar()
 joke()
